884c73772d5715ee9db3cf1a664cac4f662f821e
Rajath Prabhakar <rajath.prabhakar100@gmail.com>
2023-10-10 13:18:26 -0400

    1:18

--- Results/covid_data_merging.R
+++ Results/covid_data_merging.R
@@ -21,12 +21,13 @@ new_crosswalk <- crosswalk %>% select(c(FIPS, MSA_Code, MSA_Title))
 
 cases <- left_join(covid_cases, new_crosswalk, by = "FIPS") %>%
   mutate(across(starts_with("new_crosswalk"), ~ ifelse(is.na(FIPS), "NA", .))) %>% 
-  select(UID, iso2, iso3, code3, FIPS, MSA_Code, MSA_Title, everything())
+  select(UID, iso2, iso3, code3, FIPS, MSA_Code, MSA_Title, everything()) 
+  
 cases <- cases %>% 
   gather(key = "Date", value = "Value", "1.22.20":ncol(cases)) %>% 
   select(Date, everything()) 
 cases$Date <- as.Date(cases$Date, format = "%m.%d.%y")
-cases <- cases %>% rename(Cases = Value)
+cases <- cases %>% rename(Cases = Value) 
 
 deaths <- left_join(covid_deaths, new_crosswalk, by = "FIPS") %>% 
   mutate(across(starts_with("new_crosswalk"), ~ ifelse(is.na(FIPS), "NA", .))) %>% 
@@ -46,67 +47,6 @@ rank_avg <- read_csv("Results/CSV Files/Average_City_Rank.csv")
 table1 <- read_csv("Results/CSV Files/table_of_average_acf_lag.csv")
 combined <- read_csv("Results/CSV Files/combined_data.csv")
 
-#Average Correlation
-
-#Positive
-ggplot(table1, aes(x = reorder(Variable,
-                               abs(Average_ACF_positive)),
-                   y = Average_ACF_positive))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average ACF",
-       y = "Variable",
-       title = "Variables by Average Correlations (Positive Lags)")
-
-#Negative
-ggplot(table1, aes(x = reorder(Variable, -abs(Average_ACF_negative)),
-                   y = Average_ACF_negative))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average ACF",
-       y = "Variable",
-       title = "Variables by Average Correlations (Negative Lags)")
-
-
-#Average Rank
-
-#Positive
-ggplot(rank_avg, aes(x = reorder(Variable, -abs(avg_rank_pos)),
-                     y = avg_rank_pos))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average Rank",
-       y = "Variable",
-       title = "Variables by Average Rank (Positive Lags)")
-
-#Negative
-ggplot(rank_avg, aes(x = reorder(Variable, -abs(avg_rank_neg)),
-                     y = avg_rank_neg))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average Rank",
-       y = "Variable",
-       title = "Variables by Average Rank (Negative Lags)")
-
-#Average Lag
-
-#Positive
-ggplot(table1, aes(x = reorder(Variable, Average_Lag_positive), 
-                   y = Average_Lag_positive))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average Lag",
-       y = "Variable",
-       title = "Variables by Average Lag (Positive Lags)")
-
-#Negative
-ggplot(table1, aes(x = reorder(Variable, Average_Lag_negative), 
-                   y = Average_Lag_negative))+
-  geom_bar(stat = "identity")+
-  coord_flip()+
-  labs(x = "Average Lag",
-       y = "Variable",
-       title = "Variables by Average Lag (Negative Lags)")
 
 top12variables <- c("mean_illness", "mean_health", "mean_family", "mean_Tone",
                     "mean_fatigue", "mean_Analytic", "mean_allnone", "mean_emo_pos",
@@ -124,25 +64,72 @@ ggplot(top_12, aes(x = Lag, y = Max_ACF))+
 
 
 
-health_illness <- top_12 %>% 
-  filter(Variable == "mean_health" | Variable == "mean_illness")
-
-atlanta <- read_csv("Daily Data/atlanta_daily.csv") %>% 
-  select(Date, mean_health, mean_illness) %>% 
-  mutate(MSA_Code = "C1206")
-
 MSA_Codes <- c("C1206", "C1242", "C1446", "C1698", "C2642", "C3108", "C3498", "C3562", "C3674", 
                "C3798", "C3806", "C3890", "C3958", "C4174", "C4186", "C4266", "C4118", "C4790")
 
 
-#For all files in "Daily Data", do the above. Then join to cases_and_Deaths
+library(readr)
+library(purrr)
 
+# Define the function that reads and processes a single file
+process_daily_file <- function(file_path, MSA_Code) {
+  data <- read_csv(file_path) %>%
+    select(Date, mean_health, mean_illness) %>%
+    mutate(MSA_Code = MSA_Code)
+  return(data)
+}
+
+# Get the list of file names in the "Daily Data" folder
+file_names <- list.files("Daily Data", pattern = "*_daily.csv", full.names = TRUE)
+
+# Use map_dfr to apply the function to all files based on the MSA Codes
+data_list <- file_names %>%
+  map2_dfr(MSA_Codes, ~ process_daily_file(.x, .y))
+
+# Print or use data_list as needed
+combined_reddit_df <- bind_rows(data_list)
 
 #For cases_and_deaths, filter by MSA_Code, then group_by Date and summarise mean cases and deaths
 cases_and_deaths1 <- cases_and_deaths %>% 
   filter(MSA_Code %in% MSA_Codes) %>%
   group_by(MSA_Code, MSA_Title, Date) %>% 
-  summarise(Cases = round(mean(Cases), 2),
-            Deaths = round(mean(Deaths), 2)) %>% 
+  summarise(Cases = round(sum(Cases), 0),
+            Deaths = round(sum(Deaths), 0)) %>%
+  mutate(Daily_Cases = Cases - lag(Cases, default = 0),
+         Daily_Deaths = Deaths - lag(Deaths, default = 0),
+         Daily_Cases7 = round(rollmean(Daily_Cases, k = 7, fill = NA),2),
+         Daily_Deaths7 = round(rollmean(Daily_Deaths, k = 7, fill = NA), 2)) %>% 
   select(Date, everything())
+
+reddit_and_cases <- left_join(cases_and_deaths1, combined_reddit_df) %>% na.omit()
+#fwrite(reddit_and_cases, paste("Results/CSV Files/reddit_and_cases_deaths.csv"))
+
+reddit_and_cases <- read_csv("Results/CSV Files/reddit_and_cases_deaths.csv")
+reddit_and_cases1 <- reddit_and_cases %>%
+  group_by(MSA_Code) %>%
+  mutate(Scaled_DailyCases7 = round(scale(Daily_Cases7), 2),
+         Scaled_DailyDeaths7 = round(scale(Daily_Deaths7), 2),
+         Scaled_mean_health = round(scale(mean_health), 2),
+         Scaled_mean_illness = round(scale(mean_illness), 2))
+
+ggplot(reddit_and_cases1, aes(x = Date))+
+  geom_line(aes(y = Scaled_DailyCases7, color = "Cases"))+
+  geom_line(aes(y = Scaled_mean_health, color = "mean_health"))+
+  facet_wrap(~ MSA_Title, ncol = 3)+
+  scale_color_manual(values = c("Cases" = "blue", "mean_health" = "red"))+
+  labs(x = "Date",
+       y = "Z Score",
+       title = "Time Series of mean_Health vs Cases")
+
+ggplot(reddit_and_cases1, aes(x = Date))+
+  geom_line(aes(y = Scaled_DailyCases7, color = "Cases"))+
+  geom_line(aes(y = Scaled_mean_illness, color = "mean_illness"))+
+  facet_wrap(~ MSA_Title, ncol = 3)+
+  scale_color_manual(values = c("Cases" = "blue", "mean_illness" = "orange"))+
+  labs(x = "Date",
+       y = "Z Score",
+       title = "Time Series of mean_illness vs Cases")
+
+
+mean(reddit_and_cases$Daily_Cases7)
 
